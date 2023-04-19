b890b6ac5c9c1ed2d99ed8730e9bc011154bc597
Paul Love <paul@anglepoised.com>
2023-04-19 09:13:01 +0100

    build: minify prod css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  plugins: [require("postcss-import"), require("autoprefixer")],
+  plugins:
+    process.env.NODE_ENV === "production"
+      ? [require("autoprefixer"), require("cssnano"), require("postcss-import")]
+      : [require("autoprefixer"), require("postcss-import")],
 };
