b21f435e103cf8b7cf57e7886794228edae48c7e
Paul Love <paul@anglepoised.com>
2023-05-24 09:25:57 +0100

    feat: prevent indexing of lighthouse reports

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/*.svg");
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   eleventyConfig.addPassthroughCopy("./src/favicon.png");
+  eleventyConfig.addPassthroughCopy("./src/robots.txt");
   eleventyConfig.addPassthroughCopy("./src/social.png");
 
   return {
