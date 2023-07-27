b5a890bea55d2e424b4e5dd391f356c8cfc032bd
Chris Villa <chrisvilla@me.com>
2023-07-27 18:23:24 +0100

    fix: use env var for supabase URL

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://oettsntaymnxofmmsfip.supabase.co";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
