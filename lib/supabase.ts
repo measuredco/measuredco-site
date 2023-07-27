ba12df8f9c43540f9968b088a2af9281927342dd
Chris Villa <chrisvilla@me.com>
2023-07-27 17:49:10 +0100

    build: update env vars to align with vercel supabase plugin

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://oettsntaymnxofmmsfip.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
