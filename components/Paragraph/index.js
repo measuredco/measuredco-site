54505cec8b6cde074273f0804f0e73161aeb1331
Chris Villa <chrisvilla@me.com>
2023-08-15 13:31:45 +0100

    feat: add maxWidth prop to Heading and Paragraph

--- components/Paragraph/index.js
+++ components/Paragraph/index.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./Paragraph.css";
 
-const Paragraph = ({ children, desktopSize, size = "03" }) => {
+const Paragraph = ({ children, desktopSize, size = "03", maxWidth }) => {
   return (
     <p
       className={`msrd-Paragraph ${size ? `msrd-Paragraph--${size}` : ""} ${
         desktopSize ? `msrd-Paragraph--desktop${desktopSize}` : ""
       }`}
+      style={{ maxWidth }}
     >
       {children}
     </p>
