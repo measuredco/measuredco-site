2d295741de5331d6b76950f7b4ad4238290a885c
monospaced <scott@monospaced.com>
2023-10-02 14:18:34 +0100

    feat: add link prop to Card

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,19 +1,28 @@
+import classNames from "classnames";
+import Link from "next/link";
 import React from "react";
 
 import { Artifact, Heading, Space } from "../";
 
 import "./Cards.css";
 
-export const Card = ({ artifact, headingLevel, title, description }) => {
+export const Card = ({ artifact, headingLevel, link, title, description }) => {
+  const CardElement = link ? Link : "div";
+
   return (
-    <div className="msrd-Card">
-      <Artifact>{artifact}</Artifact>
+    <CardElement
+      className={classNames({ "msrd-Card": true, ["msrd-Card--link"]: link })}
+      href={link}
+    >
+      <div className="msrd-Card-artifact">
+        <Artifact>{artifact}</Artifact>
+      </div>
       <Heading level={headingLevel} size="05">
         {title}
       </Heading>
       <Space size="03" />
       <p>{description}</p>
-    </div>
+    </CardElement>
   );
 };
 
