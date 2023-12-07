d555d1afd4e523cdcf91b81aa936b53ad015fd58
monospaced <scott@monospaced.com>
2023-12-07 10:46:14 +0000

    chore: fix feeds

--- app/api/feed/atom/route.ts
+++ app/api/feed/atom/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 
 import { generateFeed } from "../../../../lib/generate-feed";
 
+export const dynamic = "force-dynamic";
 export const revalidate = 86400;
 
 export async function GET() {
