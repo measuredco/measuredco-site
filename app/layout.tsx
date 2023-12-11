114dbbe148ce318800ea9d4f61cdb58930021603
monospaced <scott@monospaced.com>
2023-12-11 14:49:46 +0000

    feat: update favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
         />
         <meta itemProp="image" content="https://measured.co/social.png" />
         <meta name="image" content="https://measured.co/social.png" />
+        <meta name="theme-color" content="#a81a66" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content="https://measured.co/social.png" />
         <meta
@@ -40,8 +41,10 @@ export default function RootLayout({
           title="Measured - JSON Feed"
         ></link>
         <link rel="canonical" href="https://measured.co" />
-        <link rel="icon" href="favicon.png" type="image/png" />
-        <link rel="icon" href="favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <script
           defer
           data-domain="measured.co"
