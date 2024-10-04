538175832349ab015cdc08c4e2312e0d74c3da70
Chris Villa <chrisvilla@me.com>
2024-10-04 14:56:39 +0100

    build: add missing lib

---
+++ lib/get-page-res.ts
@@ -0,0 +1,10 @@
+import { supabase } from "./supabase";
+import { UserData } from "../puck.config";
+
+export const getPageRes = async (path: string) =>
+  (await supabase.from("puck").select("*").eq("path", path).maybeSingle()) as {
+    status: number;
+    data?: {
+      data: UserData;
+    };
+  };
