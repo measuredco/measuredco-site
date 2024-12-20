568f918135682c9e892848e3bd8c12cc0bc050d7
monospaced <scott@monospaced.com>
2024-12-20 11:04:15 +0000

    feat: close `SiteHeaderMenu` on current page link click

--- components/SiteHeader/components/Menu/index.tsx
+++ components/SiteHeader/components/Menu/index.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import SiteHeaderCallToAction from "../CallToAction";
 
@@ -12,45 +13,49 @@ const SiteHeaderMenu = ({
   links: { current: boolean; href: string; label: string }[];
   menuOpen: boolean;
   setMenuOpen: any;
-}) => (
-  <div
-    className={classNames({
-      "msrd-SiteHeaderMenu": true,
-      "msrd-SiteHeaderMenu--open": menuOpen,
-    })}
-    onClick={(event) => {
-      const element = event.target as HTMLElement;
+}) => {
+  const pathname = usePathname();
 
-      if (window.matchMedia("(min-width: 48em)").matches) {
-        return;
-      }
-      if (
-        element.tagName === "A" &&
-        element.getAttribute("href").startsWith("#")
-      ) {
-        setMenuOpen(false);
-      }
-    }}
-  >
-    <ul className="msrd-SiteHeader-navItems">
-      {links.map(({ current, href, label }, idx) => (
-        <li key={href + idx}>
-          <Link
-            className={classNames({
-              "msrd-SiteHeader-link": true,
-              "msrd-SiteHeader-link--current": current,
-            })}
-            href={href}
-          >
-            {label}
-          </Link>
+  return (
+    <div
+      className={classNames({
+        "msrd-SiteHeaderMenu": true,
+        "msrd-SiteHeaderMenu--open": menuOpen,
+      })}
+      onClick={(event) => {
+        const element = event.target as HTMLElement;
+
+        if (window.matchMedia("(min-width: 48em)").matches) {
+          return;
+        }
+        if (
+          element.tagName === "A" &&
+          element.getAttribute("href") === pathname
+        ) {
+          setMenuOpen(false);
+        }
+      }}
+    >
+      <ul className="msrd-SiteHeader-navItems">
+        {links.map(({ current, href, label }, idx) => (
+          <li key={href + idx}>
+            <Link
+              className={classNames({
+                "msrd-SiteHeader-link": true,
+                "msrd-SiteHeader-link--current": current,
+              })}
+              href={href}
+            >
+              {label}
+            </Link>
+          </li>
+        ))}
+        <li>
+          <SiteHeaderCallToAction />
         </li>
-      ))}
-      <li>
-        <SiteHeaderCallToAction />
-      </li>
-    </ul>
-  </div>
-);
+      </ul>
+    </div>
+  );
+};
 
 export default SiteHeaderMenu;
