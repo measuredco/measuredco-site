81ab04092cb6456f5fda133e0e3bac4da40ae4a2
monospaced <scott@monospaced.com>
2025-03-13 18:23:18 +0000

    chore: content amend

--- components/SiteFooter/index.tsx
+++ components/SiteFooter/index.tsx
@@ -107,7 +107,7 @@ const SiteFooter = () => (
           <a href="/contact">Get in touch</a>
         </li>
         <li>
-          <a href="/feed.atom">Subscribe via RSS feed</a>
+          <a href="/feed.atom">Subscribe to RSS feed</a>
         </li>
       </ul>
       <div className="msrd-SiteFooter-logo">
