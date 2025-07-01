c9bea0baa7445dd416ccfebab59cacbc105c8680
monospaced <scott@monospaced.com>
2025-07-01 14:39:19 +0100

    feat: logical properties

--- components/Profile/index.tsx
+++ components/Profile/index.tsx
@@ -28,7 +28,13 @@ const Profile = ({ description, image, link, name, title }: ProfileProps) => {
           {title}
         </Paragraph>
         <Heading level="3" size="2">
-          {link ? <a href={link}>{name}</a> : <>{name}</>}
+          {link ? (
+            <a href={link} translate="no">
+              {name}
+            </a>
+          ) : (
+            <span translate="no">{name}</span>
+          )}
         </Heading>
         <Space size="03" />
         <Paragraph>{description}</Paragraph>
