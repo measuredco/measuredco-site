8277d73bc0a3c2275545b218404211f4b0cc4a69
monospaced <scott@monospaced.com>
2025-12-23 16:24:53 +0000

    feat: tweak favicon setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           href="/feed.json"
           title="Measured - JSON Feed"
         ></link>
-        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <script
